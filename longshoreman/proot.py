8d8a48506bc4ad2b2e641f04154966317b3ee5ed
Gabriel Grant <g@briel.ca>
2013-02-21 12:30:50 -0800

    move mkdir_p() from proot.py to utils.py

--- longshoreman/proot.py
+++ longshoreman/proot.py
@@ -31,26 +31,6 @@ MK_DIRS = [
 
 
 
-def mkdir_p(newdir):
-    """ from http://code.activestate.com/recipes/82465-a-friendly-mkdir/
-        works the way a good mkdir should :)
-        - already exists, silently complete
-        - regular file in the way, raise an exception
-        - parent directory(ies) does not exist, make them as well
-    """
-    if os.path.isdir(newdir):
-        pass
-    elif os.path.isfile(newdir):
-        raise OSError("a file with the same name as the desired " \
-                      "dir, '%s', already exists." % newdir)
-    else:
-        head, tail = os.path.split(newdir)
-        if head and not os.path.isdir(head):
-            mkdir_p(head)
-        #print "_mkdir %s" % repr(newdir)
-        if tail:
-            os.mkdir(newdir)
-
 def create_proot(fakeroot='/tmp/fakeroot'):
     def in_fakeroot(path):
         fakeroot_path = '/'.join([fakeroot, path])
--- longshoreman/utils.py
+++ longshoreman/utils.py
@@ -5,6 +5,7 @@
 Misc. utility functions
 """
 
+import os
 import socket
 
 def get_free_port(exclude=None):
@@ -18,3 +19,24 @@ def get_free_port(exclude=None):
       if port not in exclude:
           break
     return port
+
+def mkdir_p(newdir):
+    """ from http://code.activestate.com/recipes/82465-a-friendly-mkdir/
+        works the way a good mkdir should :)
+        - already exists, silently complete
+        - regular file in the way, raise an exception
+        - parent directory(ies) does not exist, make them as well
+    """
+    if os.path.isdir(newdir):
+        pass
+    elif os.path.isfile(newdir):
+        raise OSError("a file with the same name as the desired " \
+                      "dir, '%s', already exists." % newdir)
+    else:
+        head, tail = os.path.split(newdir)
+        if head and not os.path.isdir(head):
+            mkdir_p(head)
+        #print "_mkdir %s" % repr(newdir)
+        if tail:
+            os.mkdir(newdir)
+
