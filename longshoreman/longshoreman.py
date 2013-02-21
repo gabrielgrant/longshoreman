503019d146af02230f5d689ec7091def57b55ed1
Gabriel Grant <g@briel.ca>
2013-02-20 19:13:28 -0800

    add shared validation for container IDs

--- longshoreman/longshoreman.py
+++ longshoreman/longshoreman.py
@@ -39,12 +39,8 @@ class LongShoreMan(object):
             self.containers_dir = containers_dir
         self.processes = {}  # PID -> (stdin, stdout, stderr)
     def create_container(self, container_id, config):
-        if not conatiner_id.isalnum():
-            raise ValueError('container_id can only be alpha-numeric')
-        
-        
-        # config = {base_env, ports, source_template, etc})
-        # mkdirs
+        validate_potential_container_id(container_id)
+        # create base container
         # write config file
         subprocess.call_check(['mkenv', container_id])
 
@@ -72,5 +68,23 @@ class LongShoreMan(object):
         pass
     def _get_container_dir(self, container_id):
         return os.path.join(self.containers_dir, container_id)
+    def _validate_container_id(self, container_id):
+        # check that the name is valid
+        if not conatiner_id.isalnum():
+            raise ValueError('container_id can only be alpha-numeric')
+
+    def _validate_potential_container_id(self, container_id):
+        # check that the name is valid, and that no such container exists
+        self._validate_container_id(container_id)
+        if container_id in self.list_containers():
+            raise ValueError(
+                'A container with ID %s already exists' % container_id)
+
+    def _validate_existing_container_id(self, container_id):
+        # check that the name is valid, and that the container exists
+        self._validate_container_id(container_id)
+        if container_id not in self.list_containers():
+            raise ValueError(
+                'No container with ID %s exists' % container_id)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
